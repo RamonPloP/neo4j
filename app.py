b1ec9b0a9ee0976c83a93753dec29a3bcad33c61
Diego Martínez <a353198@uach.mx>
2023-11-28 23:20:19 -0600

    - flash import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
 from datetime import datetime
